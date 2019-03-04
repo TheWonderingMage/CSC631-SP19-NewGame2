a7370ed10cc66758719938f13d723f28297a2cce
Unknown <leibhaftige@hotmail.com>
2019-03-04 06:07:34 -0800

    WIP: :MainMenu with BG Mask
    Main menu with a BG Mask to hide transition of BG Image

---
+++ FloodedGrounds/Assets/Scripts/MenuBGController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBGController : MonoBehaviour
+{    
+    public float displayTime;
+    public Image[] imageArray;
+
+    private Animator anim;
+    private bool BGMaskSet = false;
+    private int index = 0;
+    private int lastIndex = 0;  
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!anim.GetBool("ChangingBGImg"))
+        {
+            anim.SetBool("ChangingBGImg", true);
+            index++;
+            if (index > imageArray.Length) index = 0;
+        } else
+        {
+            anim.SetBool("ChangingBGImg", false);
+        }
+    }
+
+}
