f98ae5292ceee867e156d341ed131eed1d228ba7
Travis <leibhaftige@hotmail.com>
2019-03-05 20:47:14 -0800

    Main Menus Prototype

---
+++ FloodedGrounds/Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
