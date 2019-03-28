8f291db9a33d1bc361acb1033f3adbb3124f635e
bhaveshc20 <bhaveshc789@gmail.com>
2019-03-28 14:02:08 -0700

    added Weapon, reload and walking animation and raycast script

---
+++ FloodedGrounds/Assets/Flooded_Grounds/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int bulletsPerMag = 30;
+    public int bulletsLeft;
+
+    public float fireRate = 0.1f;
+
+    float fireTimer;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+        if(fireTimer < fireRate)
+        {
+            fireTimer += Time.deltaTime;
+        }
+    }
+
+    private void Fire()
+    {
+        if (fireTimer < fireRate) return;
+        Debug.Log("Fired");
+    }
+}
