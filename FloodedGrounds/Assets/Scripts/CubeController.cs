16565de8869bf302036bf9fe8014d2c24c2836f8
Travis <leibhaftige@hotmail.com>
2019-04-04 13:57:45 -0700

    Animation Based Trigger
    Sound trigger based on animation events

---
+++ FloodedGrounds/Assets/Scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.ResetTrigger("PlayAnimation");
+        if (Input.GetAxisRaw("Jump") > 0)
+        {
+            Debug.Log("Jump!");
+            anim.SetTrigger("PlayAnimation");
+            
+        }
+        
+
+    }
+}
